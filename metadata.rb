6d9c69bb45765edba899e5bc1660a6094a15a799
Dmitry Medvedev <seneth@gmail.com>
2015-03-26 15:15:31 +0300

    remove Chef12 attributes for a while

--- metadata.rb
+++ metadata.rb
@@ -4,8 +4,6 @@ maintainer_email 'nath.e.will@gmail.com'
 license          'apache2'
 description      'modern, resource-driven cookbook for managing haproxy'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-source_url       'https://github.com/nathwill/chef-haproxy-ng'
-issues_url       'https://github.com/nathwill/chef-haproxy-ng/issues'
 version          '0.2.8'
 
 %w( fedora redhat centos scientific ubuntu ).each do |platform|
